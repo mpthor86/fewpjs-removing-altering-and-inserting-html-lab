4cf06d6399ad6d610290c9525ad18bbd40bad18d
michael <mpthornton86@gmail.com>
2021-01-26 16:08:55 -0700

    commit tree 641330468facb0591954cb985ad5b344f9beec9e

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove();
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+let message = document.querySelector('h1#victory');
+message.innerHTML = 'Michael is the champion';
